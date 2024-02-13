f6c6d0adb439be3b78fd750f8c521168e583ba9d
dileep <deelipkumar261997@gmail.com>
2024-02-13 15:52:04 +0530

    Added code in api-app

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -20,19 +20,5 @@ function App() {
 
 export default App;
 
-// import React from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Home from './routes/Home';
-// import Home1 from './routes/Home1';
 
-// function App() {
-//     return (
-//         <Router>
-//             <Route path="/routes/Home1" component={Home1} />
-//             <Route exact path="/" component={Home} />
-//         </Router>
-//     );
-// }
-
-// export default App;
 
