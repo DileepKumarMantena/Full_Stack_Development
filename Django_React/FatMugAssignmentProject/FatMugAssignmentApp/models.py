c4d3af0bb365df23d7af2366eb0beec7e219533a
DileepKumarMantena <deelipkumar261997@gmail.com>
2024-04-27 09:12:50 +0530

    Made changes in fatmug projet

--- Django_React/FatMugAssignmentProject/FatMugAssignmentApp/models.py
+++ Django_React/FatMugAssignmentProject/FatMugAssignmentApp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+class Vendor(models.Model):
+    name = models.CharField(max_length=255)
+    contact_details = models.TextField()
+    address = models.TextField()
+    vendor_code = models.CharField(max_length=50, unique=True)
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+    objects = models.Manager
+
+    class Meta:
+        db_table = "Vendor_Table"
+
+class PurchaseOrder(models.Model):
+    po_number = models.CharField(max_length=50, unique=True)
+    vendor = models.ForeignKey('Vendor', on_delete=models.CASCADE)
+    order_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=50)
+    quality_rating = models.FloatField(null=True, blank=True)
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField(null=True, blank=True)
+    objects = models.Manager
+
+    class Meta:
+        db_table = "PurchaseOrder_Table"
+
