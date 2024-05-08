7e4b815ce75dfe67431bfd2aa0b977f04a96e64b
DileepKumarMantena <dileep@nuevesolutions.com>
2024-05-08 15:47:13 +0530

    modified code in django

--- Django_React/DjangoApiProject/DjangoApiApp/urls.py
+++ Django_React/DjangoApiProject/DjangoApiApp/urls.py
@@ -8,12 +8,13 @@ from . import views
 
 # myapp/urls.py
 from django.urls import path
-from . import views
+from .views import MyModelListView, MyModelDetailView, MyModelCreateView, MyModelUpdateView, MyModelDeleteView
 
 urlpatterns = [
-    path('', views.my_model_list, name='my_model_list'),
-    path('<int:pk>/', views.my_model_detail, name='my_model_detail'),
-    path('new/', views.my_model_create, name='my_model_create'),
-    path('<int:pk>/edit/', views.my_model_update, name='my_model_update'),
-    path('<int:pk>/delete/', views.my_model_delete, name='my_model_delete'),
+    path('', MyModelListView.as_view(), name='my_model_list'),
+    path('<int:pk>/', MyModelDetailView.as_view(), name='my_model_detail'),
+    path('new/', MyModelCreateView.as_view(), name='my_model_create'),
+    path('<int:pk>/edit/', MyModelUpdateView.as_view(), name='my_model_update'),
+    path('<int:pk>/delete/', MyModelDeleteView.as_view(), name='my_model_delete'),
 ]
+
--- Django_React/DjangoApiProject/DjangoApiApp/views.py
+++ Django_React/DjangoApiProject/DjangoApiApp/views.py
@@ -1,69 +1,31 @@
-# from django.http import HttpResponse
-# from django.template import loader
-# from django.shortcuts import render
-
-
-# def members(request):
-#   template = loader.get_template('first.html')
-#   return HttpResponse(template.render())
-
-# def submit(request):
-#     if request.method == 'POST':
-#         # Capture form data
-#         first_name = request.POST.get('firstName')
-#         last_name = request.POST.get('lastName')
-#         age = request.POST.get('age')
-#         city = request.POST.get('city')
-#         state = request.POST.get('state')
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-        
-#         # Pass form data to template
-#         context = {
-#             'first_name': first_name,
-#             'last_name': last_name,
-#             'age': age,
-#             'city': city,
-#             'state': state,
-#             'username': username,
-#             'password': password,
-#         }
-#         # Render a separate HTML page to display the submitted data
-#         return render(request, 'submitted_form.html', context)
-#     else:
-#         # Handle GET request (e.g., display the form)
-#         return render(request, 'first.html')
-
 # myapp/views.py
-from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import MyModel
 
-def my_model_list(request):
-    my_models = MyModel.objects.all()
-    return render(request, 'my_model_list.html', {'my_models': my_models})
+class MyModelListView(ListView):
+    model = MyModel
+    template_name = 'my_model_list.html'
+    context_object_name = 'my_models'
 
-def my_model_detail(request, pk):
-    my_model = get_object_or_404(MyModel, pk=pk)
-    return render(request, 'my_model_detail.html', {'my_model': my_model})
+class MyModelDetailView(DetailView):
+    model = MyModel
+    template_name = 'my_model_detail.html'
+    context_object_name = 'my_model'
 
-def my_model_create(request):
-    if request.method == 'POST':
-        # Process form submission
-        return redirect('my_model_list')
-    else:
-        return render(request, 'my_model_form.html')
+class MyModelCreateView(CreateView):
+    model = MyModel
+    template_name = 'my_model_form.html'
+    fields = ['name', 'description']
+    success_url = reverse_lazy('my_model_list')
 
-def my_model_update(request, pk):
-    my_model = get_object_or_404(MyModel, pk=pk)
-    if request.method == 'POST':
-        # Process form submission
-        return redirect('my_model_detail', pk=pk)
-    else:
-        return render(request, 'my_model_form.html', {'my_model': my_model})
+class MyModelUpdateView(UpdateView):
+    model = MyModel
+    template_name = 'my_model_form.html'
+    fields = ['name', 'description']
+    success_url = reverse_lazy('my_model_list')
 
-def my_model_delete(request, pk):
-    my_model = get_object_or_404(MyModel, pk=pk)
-    if request.method == 'POST':
-        my_model.delete()
-        return redirect('my_model_list')
-    return render(request, 'my_model_confirm_delete.html', {'my_model': my_model})
+class MyModelDeleteView(DeleteView):
+    model = MyModel
+    template_name = 'my_model_confirm_delete.html'
+    success_url = reverse_lazy('my_model_list')
