15c475ab35d49fedcd9eaefa756532edc1308835
dileep13 <dileep@nuevesolutions.in>
2025-03-05 21:07:37 +0530

    added more features

---
+++ Python/interview.py
@@ -0,0 +1,20 @@
+# # name = ['Shiv', 'Harry', 'Amit', 'Chirag']
+# # age = ['32','26','10','33']
+# # output = ['Amit','Harry','Shiv', 'Chirag']
+# name = ['Shiv', 'Harry', 'Amit', 'Chirag']
+# age = ['32','26','10','33']
+# a=sorted(age)
+# print(a)
+# b=[name[2],name[1],name[0],name[3]]
+# print(b)
+
+a=[1,2,4,5,3,5,4,5,5,8,9]
+b=(sorted(a))
+print(b)
+# count =0
+for i in b:
+    print(i)
+    
+   
+
+print(b)
